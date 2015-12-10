f4c873b3c0dfa1cd3a966fea46ece72abd345320
david.onoro <david.onoro@gmail.com>
2015-12-10 17:48:04 +0100

    Esqueleto app

---
+++ src/main/java/com/pebd/rtdmp/Navigation.java
@@ -0,0 +1,7 @@
+package com.pebd.rtdmp;
+
+/**
+ * Created by david on 10/12/15.
+ */
+public class Navigation {
+}
---
+++ src/main/scala/com/pebd/rtdmp/RTDMP.scala
@@ -0,0 +1,8 @@
+package com.pebd.rtdmp
+
+/**
+  * Created by david on 10/12/15.
+  */
+object RTDMP {
+
+}
