1bcef0bef347060535f43d83edd866e3a031462d
david.onoro <david.onoro@gmail.com>
2016-01-16 00:12:43 +0100

    Final revision

--- src/test/scala/com/pebd/rtdmp/ReadFile.scala
+++ src/test/scala/com/pebd/rtdmp/ReadFile.scala
@@ -11,14 +11,15 @@ object ReadFile{
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setMaster("local[6]").setAppName("READFILE")
     val sc = new SparkContext(sparkConf)
-    val file = sc.textFile("/home/david/Descargas/raw_pageviews_day_20151203.json")
+    val file = sc.textFile("/media/david/Elements/BigData/Proyecto/data/d1/raw_pageviews_day_20151201.json")
 
     val docsRDD = file.map{line=> NavigationDataExtractor.parseData(line)}
     val sumOrderRDD = docsRDD.map(doc => (doc.getDominio,1)).
       reduceByKey(_+_).
-      filter(x=>{x._1!="null"}).
-      sortBy(-_._2)
-    sumOrderRDD.take(1000).foreach(println)
+      filter(x=>{x._1!=null && x._1!=""}).
+      sortBy(-_._2).take(1000)
+    val rddFiltrado = sc.parallelize(sumOrderRDD)
+    rddFiltrado.saveAsTextFile("file:///home/david/Descargas/data")
   }
 
 }
